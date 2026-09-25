ce5a4b6169e6e7565ea4bee7dbe3428a043f4f40
Kendall Crane <kacrane@uw.edu>
2026-09-25 15:06:12 -0700

    Revert "Fix: remove remaining mdates references"
    This reverts commit feab3482f2b2e9c10888d99f757318d525ce8c1a.

--- src/qt3utils/applications/qt3scope/application_gui.py
+++ src/qt3utils/applications/qt3scope/application_gui.py
@@ -55,7 +55,7 @@ class ScopeApplicationView:
         # Format x-axis as mm:ss for elapsed time
         ax = self.data_viewport.ax
         ax.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, p: f'{int(x//60)}:{int(x%60):02d}'))
-        self.data_viewport.ax.xaxis.set_major_formatter()
+        self.data_viewport.ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
         self.data_viewport.fig.autofmt_xdate(rotation=45, ha='right')
         self.data_viewport.canvas.draw()
 
@@ -84,7 +84,7 @@ class ScopeApplicationView:
         # Format x-axis as mm:ss for elapsed time
         ax = self.data_viewport.ax
         ax.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, p: f'{int(x//60)}:{int(x%60):02d}'))
-        self.data_viewport.ax.xaxis.set_major_formatter()
+        self.data_viewport.ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
         self.data_viewport.fig.autofmt_xdate(rotation=45, ha='right')
         self.data_viewport.canvas.draw()
 
