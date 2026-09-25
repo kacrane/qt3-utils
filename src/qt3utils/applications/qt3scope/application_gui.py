0a5d699edeb5d62ef2104291b144f6a4d6b3f9aa
Kendall Crane <kacrane@uw.edu>
2026-09-25 15:24:18 -0700

    Fix: plot with elapsed time x-axis, mm:ss formatting

--- src/qt3utils/applications/qt3scope/application_gui.py
+++ src/qt3utils/applications/qt3scope/application_gui.py
@@ -62,14 +62,19 @@ class ScopeApplicationView:
         # Clear the axis
         self.data_viewport.ax.clear()
 
-        # Plot the data line
-        self.data_viewport.ax.plot(self.application.data_y[-self.application.max_samples_to_plot:],
+        # Plot the data line with elapsed time on x-axis
+        x_data = self.application.data_x[-self.application.max_samples_to_plot:]
+        y_data = self.application.data_y[-self.application.max_samples_to_plot:]
+        self.data_viewport.ax.plot(x_data, y_data,
                                    color='k',
                                    linewidth=1.5)
 
-        self.data_viewport.ax.set_xlim(0, self.application.max_samples_to_plot)
+        if len(x_data) > 0:
+            self.data_viewport.ax.set_xlim(x_data[0], x_data[-1])
+        else:
+            self.data_viewport.ax.set_xlim(0, 1)
 
-        self.data_viewport.ax.set_xlabel(f'Sample index', fontsize=14)
+        self.data_viewport.ax.set_xlabel(f'Time (mm:ss)', fontsize=14)
         self.y_label = self.application.scope_intensity_ylabel()
         self.data_viewport.ax.set_ylabel(self.y_label, fontsize=14)
         self.data_viewport.ax.grid(alpha=0.3)
